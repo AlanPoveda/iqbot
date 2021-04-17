cda9d4f0aee7c3d9cbc63ff894238e345a331fae
AlanPoveda <alsepoveda@gmail.com>
2021-04-17 17:35:33 -0300

    ✨ Colocando a parte do martingale

--- estatistica.py
+++ estatistica.py
@@ -1,3 +1,4 @@
+from random import *
 from iqoptionapi.stable_api import IQ_Option
 import sys
 import time
@@ -24,14 +25,70 @@ else:
 
 
 
-ativo = "EURUSD-OTC"
-valor = 1
+#pares = ["EURUSD-OTC","GBPUSD-OTC", "EURGBP-OTC",'USDJPY-OTC', 'EURJPY-OTC','CADJPY-OTC','GBPJPY-OTC','AUDJPY-OTC']
+pares = ["EURUSD-OTC", "EURGBP-OTC",'EURJPY-OTC','USDCHF-OTC','GBPUSD-OTC','AUDCAD-OTC']
+numberRandom = randint(0,5)
+valor = 10
 entrada ='put'
-tempo =5
+tempo = 1
+
+
+quantidadeentradas = 0 
+acerto = 0 
+erroCont = 0
+erro = 0
+doji = 0
+
+
+par = pares[numberRandom]
+
+acertividade = 0
+
 
 while True:
-    compra_status, id_compra = API.buy(1, 'EURUSD-OTC', 'put', 5)
-    #compra_status, id_compra = API.buy_digital_spot(ativo, valor, entrada, tempo )
-    print("Status da compra: ", compra_status, id_compra)
+    print("O par que esta sendo comprado: ", par)
+    compra_status, id = API.buy(valor, par, entrada, tempo)
+    #compra_status, id_compra = API.buy_digital_spot(pares, valor, entrada, tempo )
+    print("Status da compra: ", compra_status, id)
+    result = API.check_win_v3(id)
+    if result > 0:
+        acerto += 1
+        numberRandom = randint(0,5)
+        par = pares[numberRandom]
+        valor = 10
+        acertividade = (100*acerto)/(acerto+erroCont)
+        erro = 0
+    elif result == 0:
+        doji += 0
+    elif result < 0:
+        valor = (valor * 1.15)*2
+        erro += 1
+        if erro > 2:
+            erroCont += 1
+            numberRandom = randint(0,5)
+            par = pares[numberRandom]
+            valor = 10
+            erro = 0
+            acertividade = (100*acerto)/(acerto+erroCont)
+            time.sleep(300)
+    quantidadeentradas += 1
+
+    
+    
+    print('Total de entradas :', quantidadeentradas)
+    print('Acertos: ', acerto)
+    print('Erros: ', erroCont)
+    print('Porcentagem de acertividade: ', acertividade, '%')
+    if quantidadeentradas == 100:
+        print('Total de entradas :', quantidadeentradas)
+        print('Acertos: ', acerto)
+        print('Erros: ', erro)
+        print('Empate: ', doji)
+        print('Porcentagem de acertividade: ', acertividade, '%')
+        break
+    
+    
+    
     #Tempo para executar novamente, 5 min
-    time.sleep(300)
+    #time.sleep(300)
+
