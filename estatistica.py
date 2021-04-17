3a0d7e40bb01756131d6717df0d457d7257afe30
AlanPoveda <alsepoveda@gmail.com>
2021-04-16 23:48:02 -0300

    ✨ Finalização do robô simples, somente entrada pra baixo e funcionando

--- estatistica.py
+++ estatistica.py
@@ -22,10 +22,16 @@ else:
 
 #Fazendo a compra
 
-#API.buy(2, 'EURUSD', 'put', 1) binário
+
+
+ativo = "EURUSD-OTC"
+valor = 1
+entrada ='put'
+tempo =5
 
 while True:
-    compra_status, id_compra = API.buy_digital_spot("EURUSD", 1, 'put', 5)
+    compra_status, id_compra = API.buy(1, 'EURUSD-OTC', 'put', 5)
+    #compra_status, id_compra = API.buy_digital_spot(ativo, valor, entrada, tempo )
     print("Status da compra: ", compra_status, id_compra)
     #Tempo para executar novamente, 5 min
     time.sleep(300)
