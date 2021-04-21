287e651ecad78d9b1e03506924569d8422687ab3
AlanPoveda <alsepoveda@gmail.com>
2021-04-21 20:55:11 -0300

    🔥 Removendo arquivos que não preciso

--- conectando.py
+++
@@ -1,9 +0,0 @@
-from iqoptionapi.stable_api import IQ_Option
-import logging
-
-#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
-I_want_money=IQ_Option("potatopn@gmail.com","!4lanP0veda*")
-check, reason=I_want_money.connect()#connect to iqoption
-MODE ="REAL"
-balance = I_want_money.get_balance()
-print(check, reason, balance)
