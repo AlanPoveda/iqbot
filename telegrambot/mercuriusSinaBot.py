b2d7fad7beb7e8cc3fefe6572b1c3d90688de2bd
AlanPoveda <alsepoveda@gmail.com>
2021-04-27 19:53:27 -0300

    ✨ Fazendo o do telegram e já mandando mensagem

---
+++ telegrambot/mercuriusSinaBot.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import time
+from threading import Thread, Lock
+
+# Informações do grupo, token, API
+chat_id = -356717830
+token= '1731762118:AAEBEsvGvkc2JXUYVJJOO9JnmI4IfqdYBW4'
+url_base = f'https://api.telegram.org/bot{token}/'
+resposta = requests.get("http://127.0.0.1:5000/api")
+lista = resposta.json()
+
+
+# Formatação da lista que esta sendo gerada
+formatacao = ''
+for itens in lista['lista']:
+    formatacao += f"{itens} PUT 🔻 \n"
+
+# Enviando para API do telegram
+link_de_envio = f"{url_base}sendMessage?chat_id={chat_id}&text={formatacao}"
+requests.get(link_de_envio)
+
+        
+
+
+
+
